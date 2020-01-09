be572437e1fb8fccbd5ef1d96bfb8f113a194ddf
lu5je0 <lu5je0@gmail.com>
2020-01-09 15:06:38 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,16 @@
-rm commandserver-*.jar
-cd commandserver/
-git pull origin master
-mvn package
-mv target/commandserver-*.jar ../
-cd ..
-./stop-command-server.sh
-JAR=`ls *.jar`
-nohup java -jar $JAR --spring.config.location=application.yml &
+JAR='syncbs-*.jar'
+DIR='SynCBS'
+
+rm -f $JAR
+cd $DIR/
+if [[ $1 == "-f" || `git pull origin master` != "Already up-to-date." ]]
+then
+  mvn package
+  mv target/$JAR ../
+  cd ..
+  ps -ef | grep $JAR | grep -v grep | awk '{print $2}' | xargs kill
+  JAR=`ls *.jar`
+  nohup java -Xmx600M -jar $JAR --spring.config.location=application.yml &
+else
+  echo "Already up-to-date."
+fi
