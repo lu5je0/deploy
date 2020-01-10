eee058e31456db6f537fd2e66fb199452c615472
lu5je0 <lu5je0@gmail.com>
2020-01-10 09:58:17 +0800

    bug fix

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,17 @@
-JAR='syncbs-*.jar'
-DIR='SynCBS'
+JAR='commandserver-*.jar'
+DIR='commandserver'
 
 rm -f $JAR
 cd $DIR/
 if [[ $1 == "-f" || `git pull origin master` != "Already up-to-date." ]]
 then
+  ps -ef | grep $JAR | grep -v grep | awk '{print $2}' | xargs kill
   mvn package
   mv target/$JAR ../
   cd ..
-  ps -ef | grep $JAR | grep -v grep | awk '{print $2}' | xargs kill
   JAR=`ls *.jar`
-  nohup java -Xmx600M -jar $JAR --spring.config.location=application.yml &
+  nohup java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8181 -Xmx600M -jar $JAR --spring.config.location=application.yml &
+  echo `date` `ls *.jar` >> deploy.log
 else
   echo "Already up-to-date."
 fi
